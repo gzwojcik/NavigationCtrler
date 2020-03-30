83928427fddbe4b0f7901592667c3deb15ddda7d
Grzegorz Wojcik <Gzwojcik@icloud.com>
2020-03-30 21:12:22 +0200

    Based On Sean Allen's YT channel on iOS development: https://www.youtube.com/watch?v=LbAd2FIlnos

---
+++ NavigationCtrler/HidoriViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HidoriViewController.swift
+//  NavigationCtrler
+//
+//  Created by zgaga on 30/03/2020.
+//  Copyright © 2020 Hime Electronics Int. All rights reserved.
+//
+
+import UIKit
+
+class HidoriViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Hidori Screen :)"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
